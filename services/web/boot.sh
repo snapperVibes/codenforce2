e11368b552189601a9206115a91dd368b4479ac5
Snapper <LearningWithSnapper@gmail.com>
2021-02-18 15:56:27 -0500

    Fast Api skeleton

--- services/web/boot.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Error handling
-#   standard_init_linux.go:219: exec user process caused: no such file or directory
-#     This is an error caused by Windows. Open git bash and run doc2unix boot.sh to fix
-
-echo Sleeping to wait for Postgres to wake up
-sleep 1
-python manage.py runserver 0.0.0.0:8000
--- services/web/template/README.md
+++
@@ -1,4 +0,0 @@
-## Templates
-[comment]: <> (### What is a template?)
-### Design decisions
-  - Templates that are never called directly are marked with an underscore.
